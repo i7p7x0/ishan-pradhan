5253b270ceda2b021e1bea71bbf54ca35178748d
i7p7x0 <i7p7x0@gmail.com>
2022-03-08 22:39:47 +0545

    Created logic to render about based on isDeveloper status

---
+++ application/src/pages/about/components/AboutMeForDevelopers.jsx
@@ -0,0 +1,11 @@
+import { React } from "react";
+
+const AboutMeForDevelopers = () => {
+  return (
+    <div className="about-me-for-developers-container">
+      <div>test</div>
+    </div>
+  );
+};
+
+export default AboutMeForDevelopers;
--- application/src/pages/about/page/About.jsx
+++ application/src/pages/about/page/About.jsx
@@ -1,9 +1,11 @@
-import { React } from "react";
+import { React, useState } from "react";
+import { Button } from "react-bootstrap";
 
 // CUSTOM COMPONENTS
 import AboutMe from "../components/AboutMe";
 import AboutMyProperties from "../components/AboutMyProperties";
 import AboutMyCanDos from "../components/AboutMyCanDos";
+import AboutMeForDevelopers from "../components/AboutMeForDevelopers";
 
 // DATA
 import MY_PROPERTIES from "../../../data/myProperties";
@@ -13,6 +15,20 @@ import MY_CAN_DOS from "../../../data/myCanDos";
 import "../style/about.css";
 
 const About = () => {
+  // state isDeveloper determines whether or not the user is a developer. If yes, they will be displayed technical skillset.
+  const [isDeveloper, setIsDeveloper] = useState("-1");
+
+  const handleIsDeveloperButtonPress = (event) => {
+    let pressedButton = event.target.id;
+    if (pressedButton === "developerY") {
+      setIsDeveloper("1");
+    } else if (pressedButton === "developerX") {
+      setIsDeveloper("0");
+    } else {
+      setIsDeveloper("0");
+    }
+  };
+
   return (
     <div className="about-container">
       <AboutMe />
@@ -22,6 +38,34 @@ const About = () => {
       <div className="what-i-do-container">
         <h1>What I Do</h1>
         <AboutMyCanDos dos={MY_CAN_DOS} />
+      </div>
+      <div className="about-me-for-developers-parent-container">
+        {isDeveloper === "-1" ? (
+          <div className="about-me-for-developers-choice-container">
+            <h1>Are you a developer too?</h1>
+            <div className="buttons-container">
+              <Button
+                variant="success"
+                className="button"
+                id="developerY"
+                onClick={handleIsDeveloperButtonPress}
+              >
+                Yes
+              </Button>
+
+              <Button
+                variant="danger"
+                className="button"
+                id="developerN"
+                onClick={handleIsDeveloperButtonPress}
+              >
+                No
+              </Button>
+            </div>
+          </div>
+        ) : isDeveloper === "0" ? null : isDeveloper === "1" ? (
+          <AboutMeForDevelopers />
+        ) : null}
       </div>
       <hr />
     </div>
